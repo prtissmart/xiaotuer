d547e8ed0eb39319e709106f65b2f767b4f84a3e
prtname-nnu <1048652257@qq.com>
2026-04-15 11:25:29 +0800

    123

---
+++ src/son-com.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+const props=defineProps({
+  message:String,
+  count:Number
+})
+console.log(props)
+</script>
+<template>
+<div class="son">
+  <h3>子组件Son</h3>
+  <div>
+    父组件传入的数据：{{message}}-{{ count }}
+  </div>
+</div>
+</template>
+<style scoped></style>
