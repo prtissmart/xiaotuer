43559d17f2170d9526ec0ec1f2da1c250aafe459
prtissmart <102279666+prtissmart@users.noreply.github.com>
2026-04-16 10:12:37 +0800

    day2-12scss自动导入

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,11 @@
 <template>
   <!-- 一级路由出口 -->
    <RouterView/>
+   <div class="test">test scss</div>
 </template>
-<style scoped></style>
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
+
+</style>
