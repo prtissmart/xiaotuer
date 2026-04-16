121a22f446b39f2bb9312829c6aee0e4161cb18e
prt-laptop <1048652257@qq.com>
2026-04-16 20:19:51 +0800

    day03-4面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,5 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱" />
 </template>
