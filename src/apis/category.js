6729dea1d8c8a9f60a117269654fbceb1e7c4681
prt-laptop <1048652257@qq.com>
2026-04-29 20:27:17 +0800

    day04-2二级分类面包屑导航

--- src/apis/category.js
+++ src/apis/category.js
@@ -9,3 +9,18 @@ export function getCategory(id) {
         }
     })
 }
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+
+export function getCategoryFilterAPI(id) {
+  return httpInstance({
+    url:'/category/sub/filter',
+    params:{
+      id
+    }
+  })
+}
