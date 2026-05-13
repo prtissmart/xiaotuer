e6f763804a3a2d3c0f8a99b86726ddbded1578a3
prt-laptop <1048652257@qq.com>
2026-05-13 19:45:56 +0800

    day05-2表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,18 @@
 <script setup>
-
+import { ref } from 'vue';
+//表单校验（账户名和密码）
+const form = ref({
+  account: '',
+  password: ''
+});
+// 规则
+const rules={
+    account:[
+        {required:true,message:'请输入账户名',trigger:'blur'}    ],
+    password:[
+        {required:true,message:'请输入密码',trigger:'blur'},
+        {min:6,max:14,message:'密码长度不能小于6位大于14位',trigger:'blur'}]
+}
 </script>
 
 
@@ -24,13 +37,13 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item  prop="account" label="账户">
+                <el-input v-model="form.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" type="password"/>
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
