c1dfb49700cbf274c0434b8088dd6ddc3d42fd9c
prtissmart <102279666+prtissmart@users.noreply.github.com>
2026-05-14 11:05:08 +0800

    day05-9拦截器配置token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus';
+import { useUserStore } from '@/stores/user'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -9,19 +10,26 @@ const httpInstance = axios.create({
 
 // 请求拦截器
 httpInstance.interceptors.request.use(config => {
-    return config
-  }, error => {
-    return Promise.reject(error)
-  })
+  //1.从pinia获取token数据
+  const userStore = useUserStore()
+  //2.按照后端的要求拼接token
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 httpInstance.interceptors.response.use(response => {
-    return response.data
-  }, error => {
-    ElMessage({
-      message: error.response.data.message,
-      type: 'warning',
-    });
-    return Promise.reject(error)
-  })
+  return response.data
+}, error => {
+  ElMessage({
+    message: error.response.data.message,
+    type: 'warning',
+  });
+  return Promise.reject(error)
+})
 export default httpInstance
