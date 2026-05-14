0c4115da56c2c66c9ded7d7464664d9b646941b9
prtissmart <102279666+prtissmart@users.noreply.github.com>
2026-05-14 11:29:51 +0800

    day05-11401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus';
 import { useUserStore } from '@/stores/user'
+import router from '@/router';
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -26,10 +27,20 @@ httpInstance.interceptors.request.use(config => {
 httpInstance.interceptors.response.use(response => {
   return response.data
 }, error => {
+  const userStore = useUserStore()
   ElMessage({
     message: error.response.data.message,
     type: 'warning',
   });
+  //401
+  //1.清楚本地用户数据
+  if (error.response.status === 401) {
+
+    userStore.clearUserInfo()
+    //2.跳转登录页
+    router.push('/login')
+  }
+
   return Promise.reject(error)
 })
 export default httpInstance
