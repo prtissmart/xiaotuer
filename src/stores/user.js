10f7f6f2b631e1adcf4651055aaea11fb63fe2a5
prtissmart <102279666+prtissmart@users.noreply.github.com>
2026-05-14 11:14:00 +0800

    day05-10退出登录

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,22 +1,27 @@
 import { defineStore } from "pinia";
-import {loginAPI} from '@/apis/user'
+import { loginAPI } from '@/apis/user'
 import { ref } from "vue";
 
 export const useUserStore = defineStore('user', () => {
     //1.定义管理用户数据的state
     const userInfo = ref({})
     //2.定义修改用户数据的action
-    const getUserInfo = async(account,password) => {
-        const res = await loginAPI(account,password)
+    const getUserInfo = async (account, password) => {
+        const res = await loginAPI(account, password)
         userInfo.value = res.result
         //1.调用接口获取用户数据
         //2.将用户数据保存到userInfo中
     }
+    //退出清除用户信息
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
     //3.导出这个store供组件使用
     return {
         userInfo,
-        getUserInfo
+        getUserInfo,
+        clearUserInfo
     }
-},{
+}, {
     persist: true
 })
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { useUserStore } from '@/stores/user';
 const userStore = useUserStore();
-
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const confirm = () => {
+  // console.log('退出登录');
+  // userStore.logout();
+  userStore.clearUserInfo()
+  router.push('/login')
+}
 
 </script>
 
@@ -13,7 +20,7 @@ const userStore = useUserStore();
         <template v-if="userStore.userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
